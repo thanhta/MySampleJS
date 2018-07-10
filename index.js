cc7bd226cc18cf5ea752f2fd78a7dc6b160c2f33
Ta, Thanh [GTSUS] <TTa@ITS.JNJ.COM>
2018-07-10 19:35:14 -0400

    another change index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello THis is my Sample Node.JS code!");
+    response.end("Hello Another Sample Node.JS code!");
 
 });
 
